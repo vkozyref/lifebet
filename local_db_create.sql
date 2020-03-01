7d94f828d61edbbaf56e8572c7c8f7fc72d43ca1
vkozyref <kozyrev.vitaliy@gmail.com>
2020-03-01 18:03:07 +0200

    local db setup script

---
+++ local_db_create.sql
@@ -0,0 +1,13 @@
+﻿
+# psql -U postgres --password -h localhost -p 5433 postgres -f ./local_db_create.sql
+
+CREATE USER lifebet_sa WITH PASSWORD 'pwd1';
+
+CREATE DATABASE lifebet
+   WITH OWNER lifebet_sa 
+   TEMPLATE template0
+   ENCODING 'SQL_ASCII'
+   TABLESPACE  pg_default
+   LC_COLLATE  'C'
+   LC_CTYPE  'C'
+   CONNECTION LIMIT  -1;
