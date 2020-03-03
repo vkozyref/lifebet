1d5fce768ed8b50372b752b0ac7deeb01bb497e9
vkozyref <kozyrev.vitaliy@gmail.com>
2020-03-04 00:27:44 +0200

    add tests job circle

---
+++ lifebet.tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using NUnit.Framework;
+
+namespace lifebet.tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
